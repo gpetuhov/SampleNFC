cea3cbd0b4c989ddaacb684fe98b7ab7074e0aff
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-29 17:25:31 +0300

    Show toast on NFC discovered

--- app/src/main/java/com/gpetuhov/android/samplenfc/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplenfc/MainActivity.kt
@@ -2,11 +2,54 @@ package com.gpetuhov.android.samplenfc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.app.PendingIntent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import com.pawegio.kandroid.toast
+
+// MainActivity uses Foreground Dispatch System to intercept an intent
+// and claim priority over other activities that handle the same intent.
+
+// By default NFC intent is dispatched to the application, that most fits the intent
+// (NFC technologies used, data type).
+
+// Here MainActivity does NOT have intent filter to catch NFC intents,
+// but shows toast on NFC discovered, when is RUNNING.
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nfcAdapter: NfcAdapter
+    private lateinit var pendingIntent: PendingIntent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val tag: Tag? = intent?.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+
+        if (tag != null) {
+            // This toast will show up, if MainActivity is resumed only
+            toast("NFC discovered")
+        }
     }
 }
---
+++ app/src/main/java/com/gpetuhov/android/samplenfc/NfcActivity.kt
@@ -0,0 +1,12 @@
+package com.gpetuhov.android.samplenfc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NfcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc)
+    }
+}
